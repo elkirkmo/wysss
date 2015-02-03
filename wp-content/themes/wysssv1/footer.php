9771976c4fc7fcc619e7f7b12312d057b754d0a0
Chris Kirkham <me@chriskirkham.com>
2015-02-02 23:19:23 -0800

    Removing old themes and footer
    BOO FOOTERS AND OLD THEMES

--- wp-content/themes/wysssv1/footer.php
+++ wp-content/themes/wysssv1/footer.php
@@ -12,9 +12,6 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'wysssv1' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'wysssv1' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'wysssv1' ), 'wysssv1', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
